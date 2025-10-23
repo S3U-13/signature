dbb28695accc4d5a295cc4ec6f1af75a75d9073d
S3U-13 <payaknoi003@gmail.com>
2025-10-23 18:54:45 +0700

    all form

--- app/(page)/form/create_form_1/page.jsx
+++ app/(page)/form/create_form_1/page.jsx
@@ -1,5 +1,7 @@
 "use client";
 import { Button } from "@heroui/button";
+import { DatePicker } from "@heroui/date-picker";
+import { Input, Textarea } from "@heroui/input";
 import {
   Modal,
   ModalBody,
@@ -7,37 +9,204 @@ import {
   ModalFooter,
   ModalHeader,
 } from "@heroui/modal";
+import { Radio, RadioGroup } from "@heroui/radio";
 import React from "react";
 
 export default function page({ openForm1, closeForm1, modalRef }) {
   return (
     <div>
-      <Modal isOpen={openForm1} onOpenChange={closeForm1}>
+      <Modal
+        size="3xl"
+        isOpen={openForm1}
+        onOpenChange={closeForm1}
+        classNames={{ body: "max-h-[calc(85vh-120px)] overflow-y-scroll" }}
+      >
         <ModalContent ref={modalRef}>
           {(closeForm1) => (
             <>
-              <ModalHeader className="flex flex-col gap-1">
-                form 1
+              <ModalHeader className="flex flex-col text-md gap-1">
+                หนังสืออธิบายเเละยินยอมให้ทำการจำลองการฉายรังสีโดยใช้รังสีเอกซเรย์เเละสารทึบรังสี
               </ModalHeader>
               <ModalBody>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Magna exercitation reprehenderit magna aute tempor cupidatat
-                  consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex
-                  incididunt cillum quis. Velit duis sit officia eiusmod Lorem
-                  aliqua enim laboris do dolor eiusmod. Et mollit incididunt
-                  nisi consectetur esse laborum eiusmod pariatur proident Lorem
-                  eiusmod et. Culpa deserunt nostrud ad veniam.
-                </p>
+                <h1>หน่วยงานรังสีรักษา โรงพยาบาลพระปกเกล้า</h1>
+                <div>
+                  <Input label="ชื่อ-สกุล ผู้ป่วย" />
+                  <Input label="อายุ" />
+                  <Input label="HN" />
+                  <Input label="HN" />
+                  <DatePicker
+                    isReadOnly
+                    className="max-w-[284px]"
+                    label="Birth date"
+                  />
+                  <Input label="น้ำหนัก" />
+                </div>
+                <div>
+                  <p>
+                    ท่านกำลังจะเข้ารับการตรวจทางรังสีโดยใช้รังสีเอกซเรย์
+                    หรือการฉีดสารทึบรังสีร่วมกับการเอกซเรย์
+                    ซึ่งในการตรวจนี้เเพทย์/เจ้าหน้าที่จะใช้สารทึบรังสีฉีดผ่านทางหลอดเลือดดำ
+                    หลังจากนั้นจึงเอกซเรย์ ในการตรวจดังกล่าว
+                    อาจมีโอกาสเกิดการเเพ้ต่อสารทึบรังสีได้ดังนี้
+                  </p>
+                  <span>
+                    1.เเพ้เล็กน้อย ได้เเก่ คลื่นไส้/อาเจียน จาม ผื่นคัน มีไข้
+                  </span>
+                  <span>
+                    2.เเพ้ปานกลางถึงมาก ได้เเก่ หายใจขัด ความดันโลหิตต่ำ
+                    หัวใจเต้นช้า หน้าบวม ปากบวม กล่องเสียงบวม ไตวาย ชัก
+                    หรืออาจเสียชีวิตได้
+                    อย่างไรก็ตามทางหน่วยงานรังสีรักษาได้ตามมาตรการในการป้องกันเเละรักษาอาการเเพ้ที่เกิดจากการตรวจดังกล่าว
+                    ทั้งนี้เพื่อป้องกันอันตรายที่อาจเกิดขึ้น
+                    กรุณาตอบคำถามต่อไปนี้
+                    เพื่อตรวจหาความเสี่ยงต่อการเอกซเรย์หรือฉีดสารทึบรังสี
+                  </span>
+                </div>
+                <div>
+                  <span>1.ท่านมีโรคประจำตัวดังต่อไปนี้หรือไม่</span>
+                  <div className="grid grid-cols-2 gap-2">
+                    <RadioGroup label="โรคภูมิเเพ้" orientation="horizontal">
+                      <Radio value="y">มี</Radio>
+                      <Radio value="n">ไม่มี</Radio>
+                    </RadioGroup>
+                    <RadioGroup label="โรคหอบหืด" orientation="horizontal">
+                      <Radio value="y">มี</Radio>
+                      <Radio value="n">ไม่มี</Radio>
+                    </RadioGroup>
+                    <RadioGroup label="โรคไตวาย" orientation="horizontal">
+                      <Radio value="y">มี</Radio>
+                      <Radio value="n">ไม่มี</Radio>
+                    </RadioGroup>
+                    <RadioGroup label="โรคเบาหวาน" orientation="horizontal">
+                      <Radio value="y">มี</Radio>
+                      <Radio value="n">ไม่มี</Radio>
+                    </RadioGroup>
+                    <RadioGroup label="โรคหัวใจ" orientation="horizontal">
+                      <Radio value="y">มี</Radio>
+                      <Radio value="n">ไม่มี</Radio>
+                    </RadioGroup>
+                    <RadioGroup label="โรคลมชัก" orientation="horizontal">
+                      <Radio value="y">มี</Radio>
+                      <Radio value="n">ไม่มี</Radio>
+                    </RadioGroup>
+                  </div>
+                  <RadioGroup
+                    classNames={{ label: "text-black" }}
+                    label="2.ท่านเคยได้รับการฉีดสารทึบรังสีมาก่อนหรือไม่"
+                    orientation="horizontal"
+                  >
+                    <Radio value="y">เคย</Radio>
+                    <Radio value="n">ไม่เคย</Radio>
+                    <Radio value="r">จำไม่ได้</Radio>
+                  </RadioGroup>
+                  <RadioGroup
+                    classNames={{ label: "text-black" }}
+                    label="3.ถ้าเคยตรวจท่านเเพ้สารทึบรังสีหรือไม่"
+                    orientation="horizontal"
+                  >
+                    <Radio value="y">เคย</Radio>
+                    <Radio value="n">ไม่เคย</Radio>
+                    <Radio value="r">จำไม่ได้</Radio>
+                    <Input label="ระบุอาการ" />
+                  </RadioGroup>
+                  <RadioGroup
+                    classNames={{ label: "text-black" }}
+                    label="4.ท่านมีประวัติเเพ้อาหารทะเลหรือไม่"
+                    orientation="horizontal"
+                  >
+                    <Radio value="y">มี</Radio>
+                    <Radio value="n">ไม่มี</Radio>
+                    <Input label="ระบุอาการ" />
+                  </RadioGroup>
+                  <RadioGroup
+                    classNames={{ label: "text-black" }}
+                    label="5.ท่านมีประวัติการเเพ้ยาอื่นๆอีกหรีอไม่"
+                    orientation="horizontal"
+                  >
+                    <Radio value="y">มี</Radio>
+                    <Radio value="n">ไม่มี</Radio>
+                    <Radio value="r">จำไม่ได้</Radio>
+                    <Input label="ระบุอาการ" />
+                  </RadioGroup>
+                  <span>
+                    6.ข้าพเจ้าขอรับรองว่าไม่ได้อยู่ในระหว่างตั้งครรภ์
+                    ขณะได้รับการตรวจด้วยวิธีดังกล่าว
+                  </span>
+                  <DatePicker
+                    isReadOnly
+                    className="max-w-[284px]"
+                    label="โดยประจำเดินสุดท้ายมาวันที่"
+                  />
+                </div>
+                <div>
+                  <Input label="ข้าพเจ้า" />
+                  <Input label="โดยเกี่ยวข้อง" />
+                  <Input label="ชื่อ" />
+                  <span>
+                    ได้รับทราบคำอธิบายข้องต้น
+                    รวมทั้งผลเเทรกซ้อนที่อาจเกิดขึ้นจะเกิดขึ้นจากการตรวจดังกล่าว
+                    โดยข้าพเจ้า
+                  </span>
+                  <RadioGroup label="" orientation="horizontal">
+                    <Radio value="y">ยินยอมให้ทำการตรวจ</Radio>
+                    <Radio value="n">ไม่ยินยอมให้ทำการตรวจ</Radio>
+                  </RadioGroup>
+                  <span>จีงได้ลงลายมือชื่อไว้เป็นหลักฐาน</span>
+                  <div className="gird grid-cols-1 gap-2">
+                    <span>
+                      ลงชื่อ.........................ผู้ป่วย/ตัวเเทนผู้ป่วย
+                      ปุ่มเพิ่มลานเซ็น
+                    </span>
+                    <span>(.............ชื่อ..............)</span>
+                    <span>
+                      ลงชื่อ.........................เเพทย์ เรียกมาจาก db
+                    </span>
+                    <span>(.............ชื่อ..............)</span>
+                    <span>
+                      ลงชื่อ.........................นักรังสีเเพทย์
+                      ปุ่มเพิ่มลานเซ็น
+                    </span>
+                    <span>(.............ชื่อ..............)</span>
+                    <span>
+                      ลงชื่อ.........................พยาบาล ปุ่มเพิ่มลานเซ็น
+                    </span>
+                    <span>(.............ชื่อ..............)</span>
+                    <span>
+                      ลงชื่อ.........................พยาน ปุ่มเพิ่มลานเซ็น
+                    </span>
+                    <span>(.............ชื่อ..............)</span>
+                    <DatePicker
+                      isReadOnly
+                      className="max-w-[284px]"
+                      label="วันที่"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <span>สำหรับเจ้าหน้าที่</span>
+                  <Textarea
+                    isClearable
+                    className="max-w-xs"
+                    label="Description"
+                    labelPlacement="outside"
+                    placeholder="บันทึก"
+                    variant="bordered"
+                    // eslint-disable-next-line no-console
+                    onClear={() => console.log("textarea cleared")}
+                  />
+                  <span>ลงชื่อ........................ ปุ่มเพิ่มลานเซ็น</span>
+                  <span>(.............ชื่อ..............)</span>
+                  <span>ตำเเหน่ง..............</span>
+                </div>
+                <div>
+                  <span>สำหรับเจ้าหน้าที่</span>
+
+                  <Input label="Cr" />
+                  <Input label="eGFR" />
+                  <span>(ต้องมี Cr ≤ 1.5 mg%, eGFR ≥ 45)</span>
+                  <Input label="Contrast media" />
+                  <Input label="ปริมาณ CC" />
+                </div>
               </ModalBody>
               <ModalFooter>
                 <Button color="danger" variant="light" onPress={closeForm1}>
--- app/(page)/form/create_form_2/page.jsx
+++ app/(page)/form/create_form_2/page.jsx
@@ -1,5 +1,7 @@
 "use client";
 import { Button } from "@heroui/button";
+import { DatePicker } from "@heroui/date-picker";
+import { Input } from "@heroui/input";
 import {
   Modal,
   ModalBody,
@@ -7,35 +9,82 @@ import {
   ModalFooter,
   ModalHeader,
 } from "@heroui/modal";
+import { Radio, RadioGroup } from "@heroui/radio";
 import React from "react";
 
 export default function page({ openForm2, closeForm2, modalRef }) {
   return (
     <div>
-      <Modal isOpen={openForm2} onOpenChange={closeForm2}>
+      <Modal
+        size="3xl"
+        isOpen={openForm2}
+        onOpenChange={closeForm2}
+        classNames={{ body: "max-h-[calc(85vh-120px)] overflow-y-scroll" }}
+      >
         <ModalContent ref={modalRef}>
           {(closeForm2) => (
             <>
-              <ModalHeader className="flex flex-col gap-1">form 2</ModalHeader>
+              <ModalHeader className="flex flex-col gap-1">
+                ใบรับทราบข้อมูลเเละยินยอมรับดารรักษาด้วยการฉายรังสี
+                โรงพยาบาลพระปกเกล้า
+              </ModalHeader>
               <ModalBody>
+                <div>
+                  <DatePicker label="วันที่" />
+                  <Input label="ข้าพเจ้า ชื่อ" />
+                  <Input label="มีความสัมพันธ์เป็น" />
+                  <span>เกี่ยวข้องกับผู้ป่วย</span>
+                  <Input label="ชื่อ" />
+                  <Input label="เจ็บป่วยด้วยโรค" />
+                  <span>จะต้องเข้ารักษาด้วยการฉายรังสี</span>
+                </div>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
+                  ข้าพเจ้าเเละผู้เเทนของข้าพเจ้า เข้าใจถึงวิธีการรักษาด้วยรังสี
+                  คือ
+                  การฉายรังสีด้วยเครื่องฉายภาพนอกร่างกายผ่านตัวผู้ป่วยในท่านอนบนเตียงเฉพาะ
+                  โดยต้องสามารถนอนได้อย่างสงบเป็นเวลาอย่างน้อยประมาณ 15 นาที
                 </p>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
+                  ข้าพเจ้าได้ทราบถึงประโยชน์ที่คาดว่าจะได้รับจากการรักษาด้วยรังสี
+                  เเละ ภาวะเเทรกซ้อนที่อาจเกิดจากการรักษาด้วยรังสี
+                  ทั้งที่อาจเกิดระหว่างการฉายรังสี
                 </p>
                 <p>
-                  Magna exercitation reprehenderit magna aute tempor cupidatat
-                  consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex
-                  incididunt cillum quis. Velit duis sit officia eiusmod Lorem
-                  aliqua enim laboris do dolor eiusmod. Et mollit incididunt
-                  nisi consectetur esse laborum eiusmod pariatur proident Lorem
-                  eiusmod et. Culpa deserunt nostrud ad veniam.
+                  ข้าพเจ้าเเละผู้เทนของข้าพเจ้าเข้าใจถึงข้อมูลอันเป็นประโยชน์ดังกล่าว
+                  เเละซักถามข้อมูลอันเป็นประโยชน์ต่อการตัดสินใจได้ครบถ่วนเเล้ว
                 </p>
+                <RadioGroup label="" orientation="horizontal">
+                  <Radio value="y">
+                    ตัดสินใจเข้ารับการรักษาดังกล่าว เเละ จะไม่ฟ้องร้อง
+                    เรียกร้องหรือเอาความผิดกับโรงพยาบาล
+                    รวมทั้งเเพทย์เเละเจ้าหน้าที่ผู้เกี่ยวข้อง
+                    ในผลอันไม่พึงประสงค์ที่อาจเกิดขึ้นจากการรักษาดังกล่าว
+                  </Radio>
+                  <Radio value="n">ปฏิเสธการรักษา</Radio>
+                </RadioGroup>
+                <div className="gird grid-cols-1 gap-2">
+                  <span>
+                    ผู้ให้ข้อมูล..........................ตำเเหน่ง เเพทย์ /
+                    พยาบาล ปุ่มเพิ่มลานเซ็น
+                  </span>
+                  <span>(.............ชื่อ..............)</span>
+                  <span>
+                    ผู้ให้ข้อมูล..........................ผู้ป่วย หรือ
+                    ผู้เเทนโดยชอบธรรมด้วยกฏหมาย
+                  </span>
+                  <span>(.............ชื่อ..............)</span>
+                  <span>
+                    พยานฝ่ายผู้ป่วย......................... ปุ่มเพิ่มลานเซ็น
+                  </span>
+                  <RadioGroup label="" orientation="horizontal">
+                    <Radio value="y">
+                      ไม่มีพยานฝ่ายผู้ป่วย (เนื่องจากผู้ป่วยมาคนเดียว)
+                    </Radio>
+                  </RadioGroup>
+                  <span>(.............ชื่อ..............)</span>
+                  <span>พยานฝ่ายเจ้าหน้าที่.........................</span>
+                  <span>(.............ชื่อ..............)ตำเเหน่ง</span>
+                </div>
               </ModalBody>
               <ModalFooter>
                 <Button color="danger" variant="light" onPress={closeForm2}>
